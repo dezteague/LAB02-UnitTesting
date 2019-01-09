c3f81eab9b349470a47952d9d051ea07b96623eb
Deziree' Teague <dez.teague@gmail.com>
2019-01-09 13:53:44 -0800

    wrote user interface

--- Lab02UnitTesting/Lab02UnitTesting/Program.cs
+++ Lab02UnitTesting/Lab02UnitTesting/Program.cs
@@ -6,7 +6,32 @@ namespace Lab02UnitTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Teague Bank!");
+
+            bool action = true;
+            while (action)
+            {
+                Console.WriteLine("Select on of the following actions. Enter (1,2,3,4)");
+                Console.WriteLine("1. Deposit");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. My Balance");
+                Console.WriteLine("4. Exit");
+                string operation = Console.ReadLine();
+                int userInput = Convert.ToInt32(operation);
+
+                switch (userInput)
+                {
+                    case 1:
+                        Console.WriteLine("Enter desposit amount:");
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter withdrawal amount:");
+                        break;
+                    case 3:
+                        Console.WriteLine("Your balance is");
+                        break;
+                }
+            }
         }
     }
 }
