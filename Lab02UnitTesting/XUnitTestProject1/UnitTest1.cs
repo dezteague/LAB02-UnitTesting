7cba466a3230cc6708590f44b93d8e21a0fd36f7
Deziree' Teague <dez.teague@gmail.com>
2019-01-09 23:00:55 -0800

    added comments for tests

--- Lab02UnitTesting/XUnitTestProject1/UnitTest1.cs
+++ Lab02UnitTesting/XUnitTestProject1/UnitTest1.cs
@@ -6,10 +6,12 @@ namespace XUnitTestProject1
 {
     public class UnitTest1
     {
+        //use a theory when calling in more than one paramenter
         [Theory]
         [InlineData(250)]
         [InlineData(300)]
         [InlineData(350)]
+        //test that you can deposit money in bank account
         public void CanDepositTest(decimal userDeposit)
         {
             Program.balance = 0;
@@ -20,13 +22,16 @@ namespace XUnitTestProject1
         [InlineData(750, 250)]
         [InlineData(500, 500)]
         [InlineData(350, 650)]
+        //test that you can withdraw from account
         public void CanWithdrawTest(decimal result, decimal userWithdrawal)
         {
             Program.balance = 1000;
             Assert.Equal(result, Program.Withdraw(userWithdrawal));
         }
 
+        //use fact when there is only one parameter
         [Fact]
+        //test to display current balance
         public void CanFindBalanceTest()
         {
             Program.balance = 50;
